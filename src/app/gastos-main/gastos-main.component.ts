3b8249a9784e13f47dfef23f1c162a8b5e38ce48
djarae <51816647+djarae@users.noreply.github.com>
2025-01-30 04:21:39 -0300

    update changes url gastos

--- src/app/gastos-main/gastos-main.component.ts
+++ src/app/gastos-main/gastos-main.component.ts
@@ -8,7 +8,7 @@ import { Component } from '@angular/core';
 export class GastosMainComponent {
   dataGastos : any;
   async ngOnInit() {
-    const response = await fetch('http://127.0.0.1:8080/apiGastos/GetListRegistros');
+    const response = await fetch('http://127.0.0.1:8080/registros/get');
     this.dataGastos=await response.json();
   }
 
